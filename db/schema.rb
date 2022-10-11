5c38573d4b082ec14749f72e460148bec12bbe46
jevazquezb <je.vazquezb@gmail.com>
2022-10-11 17:14:40 -0500

    Run migrations to set devise and default values

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_214634) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_174028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.decimal "price"
-    t.decimal "quantity"
+    t.decimal "price", precision: 15, scale: 2, default: "0.0"
+    t.decimal "quantity", precision: 15, scale: 2, default: "0.0"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_214634) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.decimal "quantity"
+    t.decimal "quantity", precision: 15, scale: 2, default: "0.0"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
@@ -37,10 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_214634) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.decimal "preparation_time"
-    t.decimal "cooking_time"
+    t.decimal "preparation_time", precision: 15, scale: 2, default: "0.0"
+    t.decimal "cooking_time", precision: 15, scale: 2, default: "0.0"
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,6 +51,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_214634) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
