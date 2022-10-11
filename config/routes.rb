3e38f0212f7fca179bce6025d08d7c5504308a2c
joffreynk <joyjoffrey@gmail.com>
2022-10-12 01:07:33 +0200

    add routing for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'general_shopping_list/index'
-  get 'recipes/index'
-  get 'recipes/show'
+  get '/recipes', to: "recipes#index"
+  post '/recipes/create', to: "recipes#create"
+  get '/recipes/create', to: "recipes#new"
+  get '/recipes/:id', to: "recipes#show"
   get 'recipes/public_recipes'
+  post "/recipes/remove", to: "recipes#remove"
   get 'foods/index'
   get 'foods/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
