7f024640b21c0b854735cca0a127b11f7c0b1344
jevazquezb <je.vazquezb@gmail.com>
2022-10-11 17:19:14 -0500

    Set routes for devise, sign out and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'general_shopping_list/index'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/public_recipes'
-  get 'foods/index'
-  get 'foods/show'
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'recipes#public_recipes'
+
+  get '/public_recipes', to: 'recipes#public_recipes'
 end
