95bb0033e5ad09584d1c9b3d943c33bbe165513e
jevazquezb <je.vazquezb@gmail.com>
2022-10-11 17:13:55 -0500

    Add default values to decimal-type values

--- db/migrate/20221010214634_create_recipe_foods.rb
+++ db/migrate/20221010214634_create_recipe_foods.rb
@@ -1,7 +1,7 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-      t.decimal :quantity
+      t.decimal :quantity, default: 0, precision: 15, scale: 2
       t.references :recipe, null: false, foreign_key: true
       t.references :food, null: false, foreign_key: true
 
