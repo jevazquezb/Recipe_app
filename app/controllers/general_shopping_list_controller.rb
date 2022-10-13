031e79590e689ba66e4a0dc90047dbb876943cd7
jevazquezb <je.vazquezb@gmail.com>
2022-10-12 23:08:10 -0500

    Add index method to general shopping list controller

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,3 +1,14 @@
 class GeneralShoppingListController < ApplicationController
-  def index; end
+  def index
+    @missing_food = []
+    current_user.foods.each do |food|
+      food_quantity = food.quantity
+      # food_required = food.recipe_foods.reduce(0) { |sum, recipe_food| sum + recipe_food.quantity }
+      food_required = food.recipe_foods.sum { |recipe_food| recipe_food.quantity }
+      diff = food_required - food_quantity
+      @missing_food << { name: food.name, quantity: diff, price: food.price * diff } if diff.positive?
+    end
+    @total_price = @missing_food.sum { |food| food[:price] }
+    @amount_of_items = @missing_food.length
+  end
 end
